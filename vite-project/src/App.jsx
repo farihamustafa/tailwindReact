13ee0b8f927d71deba26d80cf568f34dc40ebb9a
farihamustafa <113327384+farihamustafa@users.noreply.github.com>
2024-10-09 08:04:54 -0700

    .

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,16 +1,132 @@
 import { useState } from 'react'
 import Navbar from './components/Navbar'
+import { Carousel } from "flowbite-react";
+import FooterBottom from './components/Footer';
+import * as Yup from 'yup';
+import {Formik,Form,Field,ErrorMessage} from 'formik'
 
 
 function App() {
-  
+
 
   return (
     <>
     <Navbar/>
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    
+    <div className="h-[500px] sm:h-[500px] xl:h-[500px] 2xl:h-[500px] p-5">
+      <Carousel>
+        <img src="https://flowbite.com/docs/images/carousel/carousel-1.svg" alt="..." />
+        <img src="https://flowbite.com/docs/images/carousel/carousel-2.svg" alt="..." />
+        <img src="https://flowbite.com/docs/images/carousel/carousel-3.svg" alt="..." />
+        <img src="https://flowbite.com/docs/images/carousel/carousel-4.svg" alt="..." />
+        <img src="https://flowbite.com/docs/images/carousel/carousel-5.svg" alt="..." />
+      </Carousel>
+    </div>
+
+    <div className='grid grid-cols-2 max-[645px]:grid-cols-1 lg:grid-cols-3 max-[645px]:p-5 sm:p-2  gap-4 place-items-center py-5 max-[645px]:bg-yellow-100 sm:bg-red-100 md:bg-blue-100 lg:bg-green-100'>
+
+    
+<a href="#" class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+    <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src="https://flowbite.com/docs/images/blog/image-4.jpg" alt=""/>
+    <div class="flex flex-col justify-between p-4 leading-normal">
+        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+    </div>
+</a>
+
+
+<a href="#" class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+    <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src="https://flowbite.com/docs/images/blog/image-4.jpg" alt=""/>
+    <div class="flex flex-col justify-between p-4 leading-normal">
+        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+    </div>
+</a>
+
+
+<a href="#" class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+    <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src="https://flowbite.com/docs/images/blog/image-4.jpg" alt=""/>
+    <div class="flex flex-col justify-between p-4 leading-normal">
+        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+    </div>
+</a>
+
+<a href="#" class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+    <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src="https://flowbite.com/docs/images/blog/image-4.jpg" alt=""/>
+    <div class="flex flex-col justify-between p-4 leading-normal">
+        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+    </div>
+</a>
+
+
+    </div>
+
+
+
+    <div className=' p-10  bg-gray-200'>
+    <div className='text-2xl font-black text-center'>Contact Us</div>
+  <Formik 
+  initialValues={{
+    name:'',email:'',gender:'',message:''
+  }}
+  validationSchema={Yup.object({
+    name:Yup.string().required(),
+    email:Yup.string().email().required(),
+    gender:Yup.string().required(),
+    message:Yup.string().required(),
+  })}
+  onSubmit={(values,{ resetForm })=>{ console.log(values); resetForm()}}>
+
+    {({values})=>(
+    <Form class="max-w-sm mx-auto">
+    <div class="mb-5">
+    <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your name</label>
+    <Field type="text" name="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="abc"  />
+    <ErrorMessage name='name' className='text-red-500' component={"div"} />
+  </div>
+  {values.name !== '' &&
+  <div class="mb-5">
+    <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+    <Field type="email" name="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@flowbite.com"  />
+    <ErrorMessage name='email' className='text-red-500' component={"div"} />
+  </div>
+  }
+
+{values.email !== '' &&
+  <div class="mb-5">
+    <label  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your gender</label>
+    <Field as="select" name="gender" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"  >
+            <option>Select Gender</option><option value={"male"}>Male</option><option value={"female"}>Female</option>
+        </Field>
+        <ErrorMessage name='gender' className='text-red-500' component={"div"} />
+  </div>
+   }
+
+   
+{values.gender !== '' &&
+  <div class="mb-5">
+    <label  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your message</label>
+    <Field as="textarea" name="message" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"  >
+        </Field>
+        <ErrorMessage name='message' className='text-red-500' component={"div"} />
+  </div>
+}
+
+
+{values.name !== '' && values.email !== '' && values.gender !== '' && values.message !== '' && 
+  <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+}
+  </Form>
+)}
+</Formik>
+    </div>
+
+
+    <FooterBottom/>
+
+
     </>
   )
 }
